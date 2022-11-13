819590f90d74de9061e794caba577cdbb6b26132
Hwang2212 <andyhwangsauyee@gmail.com>
2022-11-14 00:02:36 +0800

    Ongoing: Adding Easy Localiztion

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:arrivo_web/utils/src/route_generator.dart';
+import 'generated/locale_keys.g.dart';
 
 import 'package:arrivo_web/theme/theme.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(EasyLocalization(
+      child: MyApp(),
+      supportedLocales: [Locale('en', 'US')],
+      fallbackLocale: Locale('en', 'US'),
+      path: 'assets/translations'));
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       theme: getAppTheme(AppThemes.textTheme),
       initialRoute: '/',
       onGenerateRoute: RouteGenerator.onGenerateRoute,
